32bc8ad22cfaf562f541f7a55ed1ce427b52af0c
Jula <82578948+julaluxx@users.noreply.github.com>
2025-12-22 19:47:11 +0700

    Enhance output with ASCII art and comment
    Added ASCII art and a comment to the output.

--- learn_to_c.cpp
+++ learn_to_c.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!"
+  // December 22 2025
+  std::cout << "Hello World!\n";
+
+  std::cout << "\n|￣￣￣￣￣￣￣￣￣|";
+  std::cout << "\n|   I want to eat  |";
+  std::cout << "\n|     your face.   |";
+  std::cout << "\n|__________________|";
+  std::cout << "\n(\\__/)  ||";
+  std::cout << "\n(•ㅅ•) ||";
+  std::cout << "\n/ 　 づ";
 }
