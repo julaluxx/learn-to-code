861fb06e981769546c9e851fda3499ae9cf1d93b
Jula <82578948+julaluxx@users.noreply.github.com>
2025-12-24 21:00:11 +0700

    Implement age input and conditional messages
    Added age input and conditional output based on age.

--- learn_to_c.cpp
+++ learn_to_c.cpp
@@ -5,23 +5,32 @@ int main() {
   cout << "Hello World!" << endl;
 
   string name;
+  int age;
+
   cout << "Enter your name: ";
   cin >> name;
+  cout << "Enter your age: ";
+  cin >> age;
 
-  int age = 23;
   double gpa = 3.55;
   char bloodType = 'O';
   bool living = true;
 
-  cout << "￣￣￣￣￣￣￣￣￣￣￣" << endl;
-  cout << "   My name is " << name << endl;
-  cout << "   I am " << age <<" years old." << endl;
-  cout << "   My gpa is " << gpa << endl;
-  cout << "   My blood type is " << bloodType << endl;
-  cout << "______________________" << endl;
-  cout << "(\\__/)  ||" << endl;
-  cout << "(•ㅅ•) ||" << endl;
-  cout << "/ 　 づ" << endl;
+  if (age >= 18){
+    cout << "￣￣￣￣￣￣￣￣￣￣￣" << endl;
+    cout << "   My name is " << name << endl;
+    cout << "   I am " << age <<" years old." << endl;
+    cout << "   My gpa is " << gpa << endl;
+    cout << "   My blood type is " << bloodType << endl;
+    cout << "______________________" << endl;
+    cout << "(\\__/)  ||" << endl;
+    cout << "(•ㅅ•) ||" << endl;
+    cout << "/ 　 づ" << endl;
+  } else if (age >= 100){
+      cout << "Are you sure that you are belong here..." << endl;
+  } else {
+    cout << "You do not belong here, please leave." << endl;
+  }
 
   return 0;
 }
