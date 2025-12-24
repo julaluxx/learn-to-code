18f60978db3bea06f1ca236257e1d2792b4fef77
Jula <82578948+julaluxx@users.noreply.github.com>
2025-12-24 21:30:35 +0700

    Add event listener for submit button

--- just_script.js
+++ just_script.js
@@ -1,3 +1,9 @@
 console.log("Hello World!");
 
 document.getElementById("myParagraph").innerHTML = "And I like to learn.";
+
+const submit_btn = document.getElementById("submit-button");
+
+submit_btn.addEventListener("click", () => {
+  window.alert("Submitted!");
+});
